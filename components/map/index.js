c4e5b0f0d84ea3d526450add42de2129634ed288
Nikki Sollid <nikki.sollid@gmail.com>
2021-01-05 13:23:22 +0100

    Map is now separate component

---
+++ components/map/index.js
@@ -0,0 +1,267 @@
+import MapView,{Marker,PROVIDER_GOOGLE } from 'react-native-maps';
+import React,{useEffect,useState,useContext} from 'react';
+import { View } from 'react-native';
+import * as Location from 'expo-location';
+import * as Animatable from 'react-native-animatable';
+
+export default function Map(props){
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+      //https://stackoverflow.com/questions/55973073/expo-locations-watchpositionasync-not-being-removed
+      let location = await  await Location.watchPositionAsync(
+            {
+                enableHighAccuracy: true,
+                distanceInterval: 1,
+                timeInterval: 10000
+            },
+            newLocation => {
+                let coords = newLocation.coords;
+            // this.props.getMyLocation sets my reducer state my_location
+            setLocation({
+                latitude: parseFloat(coords.latitude),
+                longitude: parseFloat(coords.longitude),
+                latitudeDelta: 0.1,
+                longitudeDelta: 0.1
+            });
+          },
+          error => console.log(error)
+        );
+      setLocation(location);
+    })();
+  }, []);
+  const _regionChange = () => {
+
+  }
+
+  var mapStyle = [
+    {
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#f5f5f5"
+        }
+      ]
+    },
+    {
+      "elementType": "labels",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#616161"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "color": "#f5f5f5"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.land_parcel",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#bdbdbd"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.neighborhood",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#eeeeee"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#757575"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#e5e5e5"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#9e9e9e"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#ffffff"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "road.arterial",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#757575"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#dadada"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#616161"
+        }
+      ]
+    },
+    {
+      "featureType": "road.local",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#9e9e9e"
+        }
+      ]
+    },
+    {
+      "featureType": "transit",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "transit.line",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#e5e5e5"
+        }
+      ]
+    },
+    {
+      "featureType": "transit.station",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#eeeeee"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#c9c9c9"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#9e9e9e"
+        }
+      ]
+    }
+  ]
+
+  return (
+    <>
+    {location && (
+    <MapView provider={PROVIDER_GOOGLE} customMapStyle={mapStyle} onRegionChange={_regionChange} initialRegion={location} style={{width: '100%',height: '100%', opacity: 1}}>
+      <Marker coordinate={location} description="You are here">
+        <View style={{width: 100,height: 100, flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Animatable.View animation="tada" duration={5000} easing="ease-out" iterationCount="infinite" style={{width: 40,height: 40,backgroundColor: 'orange',borderRadius: '100%'}}/>
+        </View>
+      </Marker>
+    </MapView>
+    )}
+    </>
+  )
+}
--- screens/gameScreen/index.js
+++ screens/gameScreen/index.js
@@ -1,7 +1,5 @@
 import React,{useEffect,useState,useContext} from 'react';
 import { View, Text,TouchableOpacity,Button,Image,Alert  } from 'react-native';
-import MapView,{Marker,PROVIDER_GOOGLE } from 'react-native-maps';
-import * as Location from 'expo-location';
 import * as Animatable from 'react-native-animatable';
 import Steps from '../../steps.js';
 import {AppContext} from '../../context/appContext';
@@ -9,6 +7,9 @@ import * as Progress from 'react-native-progress';
 import moment from 'moment';
 import AnimateNumber from 'react-native-countup'
 import CountDown from '../../components/countDown';
+import Map from '../../components/map';
+
+
 export default function GameScreen({ route, navigation }) {
   const [location, setLocation] = useState(null);
 const [errorMsg, setErrorMsg] = useState(null);
@@ -23,249 +24,6 @@ const StartRiftWalking = () => {
   }
   setState({...state,riftWalking: riftWalking,menu: null})
 }
-useEffect(() => {
-  (async () => {
-    let { status } = await Location.requestPermissionsAsync();
-    if (status !== 'granted') {
-      setErrorMsg('Permission to access location was denied');
-      return;
-    }
-    //https://stackoverflow.com/questions/55973073/expo-locations-watchpositionasync-not-being-removed
-    let location = await  await Location.watchPositionAsync(
-          {
-              enableHighAccuracy: true,
-              distanceInterval: 1,
-              timeInterval: 10000
-          },
-          newLocation => {
-              let coords = newLocation.coords;
-          // this.props.getMyLocation sets my reducer state my_location
-          setLocation({
-              latitude: parseFloat(coords.latitude),
-              longitude: parseFloat(coords.longitude),
-              latitudeDelta: 0.1,
-              longitudeDelta: 0.1
-          });
-        },
-        error => console.log(error)
-      );
-    setLocation(location);
-  })();
-}, []);
-const _regionChange = () => {
-
-}
-
-var mapStyle = [
-  {
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#f5f5f5"
-      }
-    ]
-  },
-  {
-    "elementType": "labels",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.icon",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#616161"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.text.stroke",
-    "stylers": [
-      {
-        "color": "#f5f5f5"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.land_parcel",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#bdbdbd"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.neighborhood",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#eeeeee"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#757575"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#e5e5e5"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#9e9e9e"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#ffffff"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "labels.icon",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "road.arterial",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#757575"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#dadada"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#616161"
-      }
-    ]
-  },
-  {
-    "featureType": "road.local",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#9e9e9e"
-      }
-    ]
-  },
-  {
-    "featureType": "transit",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "transit.line",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#e5e5e5"
-      }
-    ]
-  },
-  {
-    "featureType": "transit.station",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#eeeeee"
-      }
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#c9c9c9"
-      }
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#9e9e9e"
-      }
-    ]
-  }
-]
 const fadeIn = {
   from: {
     opacity: 0,
@@ -296,15 +54,7 @@ const EndRiftWalkingDialog = () => {
 }
 return (
 <View style={{width: '100%',height: '100%'}}>
-    {location && (
-    <MapView provider={PROVIDER_GOOGLE} customMapStyle={mapStyle} onRegionChange={_regionChange} initialRegion={location} coordinate={location} style={{width: '100%',height: '100%', opacity: 1}}>
-      <Marker coordinate={location} description="You are here">
-        <View style={{width: 100,height: 100, flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Animatable.View animation="tada" duration={5000} easing="ease-out" iterationCount="infinite" style={{width: 40,height: 40,backgroundColor: 'orange',borderRadius: '100%'}}/>
-        </View>
-      </Marker>
-    </MapView>
-)}
+  <Map/>
   <View style={{position: 'absolute',zIndex: 10,top:0,left:0,width: '100%',height: 150,paddingTop: 25,backgroundColor:'white',opacity: 0.7, display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
     <Text style={{fontSize: 50}}>Nikki</Text>
     <Text style={{fontSize: 12}}>Level {state.character.level}</Text>
